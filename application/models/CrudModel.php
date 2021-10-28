8a1a5df31b598507abee6759c56b4309349bba9b
Angel <mr.angelantoniolopez@gmail.com>
2021-10-27 23:03:31 -0600

    backend del carrito de reserva

---
+++ application/models/CrudModel.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CrudModel extends CI_Model{
+
+    public function __construct()
+    {
+        Parent:: __construct();
+    }
+
+    public function insertar($tabla, $item){
+        $flag = false;
+        if($this->db->insert($tabla, $item)){
+            $flag = true;
+        }
+        return $flag;
+    }
+
+    public function actualizar($id, $nombreCampoId, $tabla, $item){
+        $flag = false;
+        try {
+            $this->db->where($nombreCampoId, $id);
+            if ($this->db->update($tabla, $item)) {
+                $flag = true;
+            }else{
+                echo "error al actualizar";    
+            }
+
+        } catch (Exception $error) {
+            var_dump($error->getMessage());
+        }
+        return $flag;
+    }
+
+    public function eliminar($id, $nombreCampoId , $tabla){
+        $flag = false;
+        $this->db->where($nombreCampoId,$id);
+        if ($this->db->delete($tabla)) {
+
+        }
+        return $flag;
+    }
+    
+    public function mostrar($nombreCampoId, $tabla){
+        $this->db->order_by($nombreCampoId, "desc");
+        $query = $this->db->get($tabla);
+        return $query->result();
+    }
+
+    public function mostrarById($id, $nombreCampoId, $tabla){
+        $this->db->where($nombreCampoId, $id);
+        $query=$this->db->get($tabla);
+        return $query->row();
+    }
+
+    public function listarWhereQuery($tabla, $where){
+        $sql = $this->db->query("SELECT * FROM ".$tabla." where ".$where);
+        return $sql->result();
+    }
+
+}
