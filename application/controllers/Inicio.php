715f6ab31b0de604e69f1c0dfc48988e4f3dd666
Jose Alvaro <josealvaro.nw@gmail.com>
2021-11-15 17:14:59 -0600

    Config 4

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -6,7 +6,7 @@ class Inicio extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('Model_inicio');
+		$this->load->model('model_inicio');
 	}
 
 	public function index()
