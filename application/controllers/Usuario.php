bf2c4a2164c6ca6af503c867bcc0c6583f5a1756
Jose Alvaro <josealvaro.nw@gmail.com>
2021-11-18 18:02:09 -0600

    Fix

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -11,14 +11,18 @@ class Usuario extends CI_Controller
 
 	public function index()
 	{
-		if ($this->session->userdata("login") === TRUE) {
-			$data = array(
-				'page_title' => 'Usuario',
-				'view' => 'Usuarios/Usuario',
-				'data_view' => array(),
-				'data' => $this->Usuario_M->getUsuarios()
-			);
-			$this->load->view('Template/main_admin', $data);
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->session->userdata('tipo') == '1') {
+				$data = array(
+					'page_title' => 'Usuario',
+					'view' => 'Usuarios/Usuario',
+					'data_view' => array(),
+					'data' => $this->Usuario_M->getUsuarios()
+				);
+				$this->load->view('Template/main_admin', $data);
+			} else if ($this->session->userdata('tipo') == '2') {
+				redirect(base_url() . 'DashboardCliente', 'refresh');
+			}
 		} else {
 			redirect(base_url() . 'Login', 'refresh');
 		}
@@ -26,14 +30,18 @@ class Usuario extends CI_Controller
 
 	public function ins_Usuario()
 	{
-		if ($this->session->userdata("login") === TRUE) {
-			$data = array(
-				'page_title' => 'Usuario',
-				'view' => 'Usuarios/UsuarioInsert',
-				'data_view' => array(),
-				'data' => $this->Usuario_M->getUsuarios()
-			);
-			$this->load->view('Template/main_admin', $data);
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->session->userdata('tipo') == '1') {
+				$data = array(
+					'page_title' => 'Usuario',
+					'view' => 'Usuarios/UsuarioInsert',
+					'data_view' => array(),
+					'data' => $this->Usuario_M->getUsuarios()
+				);
+				$this->load->view('Template/main_admin', $data);
+			} else if ($this->session->userdata('tipo') == '2') {
+				redirect(base_url() . 'DashboardCliente', 'refresh');
+			}
 		} else {
 			redirect(base_url() . 'Login', 'refresh');
 		}
@@ -42,14 +50,18 @@ class Usuario extends CI_Controller
 	//obtener datos de 1 usuario
 	public function obtenerUsuario($id)
 	{
-		if ($this->session->userdata("login") === TRUE) {
-			$data = array(
-				'page_title' => 'Detalle Entidad',
-				'view' => 'Usuarios/UsuarioUpdate',
-				'data_view' => array(),
-				'detalle' => $this->Usuario_M->obtUsuario($id)
-			);
-			$this->load->view('Template/main_admin', $data);
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->session->userdata('tipo') == '1') {
+				$data = array(
+					'page_title' => 'Detalle Entidad',
+					'view' => 'Usuarios/UsuarioUpdate',
+					'data_view' => array(),
+					'detalle' => $this->Usuario_M->obtUsuario($id)
+				);
+				$this->load->view('Template/main_admin', $data);
+			} else if ($this->session->userdata('tipo') == '2') {
+				redirect(base_url() . 'DashboardCliente', 'refresh');
+			}
 		} else {
 			redirect(base_url() . 'Login', 'refresh');
 		}
@@ -58,58 +70,82 @@ class Usuario extends CI_Controller
 	//Insertar datos de 1 usuario
 	public function insertarUsuario()
 	{
-		if ($this->input->is_ajax_request()) {
-			$data = array(
-				'nombre' => $this->input->post('nombre'),
-				'cell' => $this->input->post('cell'),
-				'email' => $this->input->post('email'),
-				'pass' => base64_encode($this->input->post('pass')),
-				'tipo' => $this->input->post('tipo')
-			);
-			if ($this->Usuario_M->insertUsuario($data)) {
-				echo json_encode(array('success' => 1));
-			} else {
-				echo json_encode(array('success' => 0));
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->session->userdata('tipo') == '1') {
+				if ($this->input->is_ajax_request()) {
+					$data = array(
+						'nombre' => $this->input->post('nombre'),
+						'cell' => $this->input->post('cell'),
+						'email' => $this->input->post('email'),
+						'pass' => base64_encode($this->input->post('pass')),
+						'tipo' => $this->input->post('tipo')
+					);
+					if ($this->Usuario_M->insertUsuario($data)) {
+						echo json_encode(array('success' => 1));
+					} else {
+						echo json_encode(array('success' => 0));
+					}
+				} else {
+					echo "no se puede acceder";
+				}
+			} else if ($this->session->userdata('tipo') == '2') {
+				redirect(base_url() . 'DashboardCliente', 'refresh');
 			}
 		} else {
-			echo "no se puede acceder";
+			redirect(base_url() . 'Login', 'refresh');
 		}
 	}
 
 	//actualizar datos de 1 usuario
 	public function actualizarUsuario()
 	{
-		if ($this->input->is_ajax_request()) {
-			$data = array(
-				'nombre' => $this->input->post('nombre'),
-				'cell' => $this->input->post('cell'),
-				'email' => $this->input->post('email'),
-				'pass' => base64_encode($this->input->post('pass')),
-				'tipo' => $this->input->post('tipo'),
-				'id' => $this->input->post('id')
-			);
-			if ($this->Usuario_M->updateUsuario($data)) {
-				echo json_encode(array('success' => 1));
-			} else {
-				echo json_encode(array('success' => 0));
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->session->userdata('tipo') == '1') {
+				if ($this->input->is_ajax_request()) {
+					$data = array(
+						'nombre' => $this->input->post('nombre'),
+						'cell' => $this->input->post('cell'),
+						'email' => $this->input->post('email'),
+						'pass' => base64_encode($this->input->post('pass')),
+						'tipo' => $this->input->post('tipo'),
+						'id' => $this->input->post('id')
+					);
+					if ($this->Usuario_M->updateUsuario($data)) {
+						echo json_encode(array('success' => 1));
+					} else {
+						echo json_encode(array('success' => 0));
+					}
+				} else {
+					echo "no se puede acceder";
+				}
+			} else if ($this->session->userdata('tipo') == '2') {
+				redirect(base_url() . 'DashboardCliente', 'refresh');
 			}
 		} else {
-			echo "no se puede acceder";
+			redirect(base_url() . 'Login', 'refresh');
 		}
 	}
 
 	//borrar 1 usuario
 	public function borrarUsuario()
 	{
-		if ($this->input->is_ajax_request()) {
-			$data = array('id' => $this->input->post('id'));
-			if ($this->Usuario_M->deleteUsuarios($data)) {
-				echo json_encode(array('success' => 1));
-			} else {
-				echo json_encode(array('success' => 0));
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->session->userdata('tipo') == '1') {
+				if ($this->input->is_ajax_request()) {
+					$data = array('id' => $this->input->post('id'));
+					if ($this->Usuario_M->deleteUsuarios($data)) {
+						echo json_encode(array('success' => 1));
+					} else {
+						echo json_encode(array('success' => 0));
+					}
+				} else {
+					echo "no se puede acceder";
+				}
+			} else if ($this->session->userdata('tipo') == '2') {
+				redirect(base_url() . 'DashboardCliente', 'refresh');
 			}
 		} else {
-			echo "no se puede acceder";
+			redirect(base_url() . 'Login', 'refresh');
 		}
 	}
 
@@ -134,47 +170,55 @@ class Usuario extends CI_Controller
 	//actualizar los datos de 1 perfil de usuario
 	public function update_perfilDatos()
 	{
-		if ($this->input->is_ajax_request()) {
-			$data = array(
-				'nombre' => $this->input->post('nombre'),
-				'cell' => $this->input->post('cell'),
-				'email' => $this->input->post('email'),
-				'id' => $this->input->post('id')
-			);
-
-			if ($this->Usuario_M->update_perfilDatos($data)) {
-				$session_data = array(
-					'id' => $data['id'],
-					'nombre' => $data['nombre'],
-					'email' => $data['email'],
-					'pass' => $this->session->userdata('pass'),
-					'login' => TRUE,
-					'tipo' => $this->session->userdata('tipo')
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->input->is_ajax_request()) {
+				$data = array(
+					'nombre' => $this->input->post('nombre'),
+					'cell' => $this->input->post('cell'),
+					'email' => $this->input->post('email'),
+					'id' => $this->input->post('id')
 				);
-				$this->session->set_userdata($session_data);
-				echo json_encode(array('success' => 1));
+
+				if ($this->Usuario_M->update_perfilDatos($data)) {
+					$session_data = array(
+						'id' => $data['id'],
+						'nombre' => $data['nombre'],
+						'email' => $data['email'],
+						'pass' => $this->session->userdata('pass'),
+						'login' => TRUE,
+						'tipo' => $this->session->userdata('tipo')
+					);
+					$this->session->set_userdata($session_data);
+					echo json_encode(array('success' => 1));
+				} else {
+					echo json_encode(array('success' => 0));
+				}
 			} else {
-				echo json_encode(array('success' => 0));
+				echo "no se puede acceder";
 			}
 		} else {
-			echo "no se puede acceder";
+			redirect(base_url() . 'Login', 'refresh');
 		}
 	}
 
 	public function update_perfilPass()
 	{
-		if ($this->input->is_ajax_request()) {
-			$data = array(
-				'pass' => base64_encode($this->input->post('pass')),
-				'id' => $this->input->post('id')
-			);
-			if ($this->entidades_model->update_perfilPass($data)) {
-				echo json_encode(array('success' => 1));
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->input->is_ajax_request()) {
+				$data = array(
+					'pass' => base64_encode($this->input->post('pass')),
+					'id' => $this->input->post('id')
+				);
+				if ($this->entidades_model->update_perfilPass($data)) {
+					echo json_encode(array('success' => 1));
+				} else {
+					echo json_encode(array('success' => 0));
+				}
 			} else {
-				echo json_encode(array('success' => 0));
+				echo "no se puede acceder";
 			}
 		} else {
-			echo "no se puede acceder";
+			redirect(base_url() . 'Login', 'refresh');
 		}
 	}
 }
