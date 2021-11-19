3e86d4f9c7ba0c3edb0a7d58f32c2bfa3fe74d62
Jose Alvaro <josealvaro.nw@gmail.com>
2021-11-18 19:05:32 -0600

    Dashboard Cliente

---
+++ application/views/Administracion/DashboardCliente.php
@@ -0,0 +1,62 @@
+<div class="pagetitle">
+    <h1>Dashboard</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item active">
+                <a href="">Inicio</a>
+            </li>
+        </ol>
+    </nav>
+</div>
+
+<section class="section">
+    <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Happy Pots</h5>
+
+                    <!-- Slides with indicators -->
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="<?= base_url() . 'assets/images/menu-1-370x278.jpg'; ?>" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="<?= base_url() . 'assets/images/menu-2-370x278.jpg'; ?>" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="<?= base_url() . 'assets/images/menu-3-370x278.jpg'; ?>" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="<?= base_url() . 'assets/images/product-0-161x162.jpg'; ?>" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="<?= base_url() . 'assets/images/producto-categoria-2.jpg'; ?>" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+
+                    </div><!-- End Slides with indicators -->
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
