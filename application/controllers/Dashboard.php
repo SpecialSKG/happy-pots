610b3823eb38984766a8e6a59794de26b7112662
Jose Alvaro <josealvaro.nw@gmail.com>
2021-11-18 18:08:04 -0600

    Fix2

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -11,16 +11,20 @@ class Dashboard extends CI_Controller
 
 	public function index()
 	{
-		if ($this->session->userdata("login") === TRUE) {
-			$data = array(
-				'page_title' => 'Dashboard',
-				'view' => 'Administracion/Dashboard',
-				'data_view' => array(
-					'dataDashboard' => $this->DashboardModel->getDashboard(),
-					'dataReserva' => $this->DashboardModel->getReservas()
-				)
-			);
-			$this->load->view('Template/main_admin', $data);
+		if ($this->session->userdata('login') === TRUE) {
+			if ($this->session->userdata('tipo') == '1') {
+				$data = array(
+					'page_title' => 'Dashboard',
+					'view' => 'Administracion/Dashboard',
+					'data_view' => array(
+						'dataDashboard' => $this->DashboardModel->getDashboard(),
+						'dataReserva' => $this->DashboardModel->getReservas()
+					)
+				);
+				$this->load->view('Template/main_admin', $data);
+			} else if ($this->session->userdata('tipo') == '2') {
+				redirect(base_url() . 'DashboardCliente', 'refresh');
+			}
 		} else {
 			redirect(base_url() . 'Login', 'refresh');
 		}
